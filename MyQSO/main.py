30cae028e12b6fcde0cfd4464de6a85919a02cf3
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-02-25 21:16:13 -0700

    Update main.py

--- MyQSO/main.py
+++ MyQSO/main.py
@@ -1,6 +1,7 @@
 import tkinter
 import customtkinter as ct
 
+
 ct.set_appearance_mode("dark")  # Modes: system (default), light, dark
 ct.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
@@ -8,10 +9,7 @@ app = ct.CTk()  # create CTk window like you do with the Tk window
 app.geometry("600x400")
 app.title("MyQSO")
 
-
-# variables
-
-
+# Variables
 
 # Functions
 
@@ -21,30 +19,22 @@ def contacts_btn():
 def map_btn():
     print("Map button pressed")
 
-def call_search():
-    callsign = call_entry.get()
-    result_label = ct.CTkLabel(master=app, text=callsign, text_color="white", font=("Arial", 36))
-    result_label.place(relx=.7, rely=.7, anchor=tkinter.CENTER)
-   
-
-# Call Entry and Search
-
-call_entry = ct.CTkEntry(master=app, height=50, width=100, placeholder_text="CALL SIGN", fg_color="white", text_color="black")
-call_entry.place(relx=.4, rely=.5, anchor = tkinter.CENTER)
-
-call_button = ct.CTkButton(master=app, text="Search", command=call_search, width=50, height=50)
-call_button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+def call_search(*args):
+    if len(call_entry.get()) == 0:
+        callsign = "ERROR: CALLSIGN EMPTY"
+        result_label = ct.CTkLabel(master=app, text=callsign, text_color="RED", font=("Arial", 20))
+        result_label.place(relx=.5, rely=.75, anchor=tkinter.CENTER)
+    else:
+        callsign = call_entry.get()
+        result_label = ct.CTkLabel(master=app, text=callsign, text_color="white", font=("Arial", 26))
+        result_label.place(relx=.5, rely=.75, anchor=tkinter.CENTER)
+        
 
 
-#name_entry = ct.CTkEntry(master=app, height=10, width=150, placeholder_text="CALL SIGN", fg_color="white", text_color="black")
-#call_entry.place(relx=.2, rely=.5, anchor = tkinter.CENTER)
-
-
-# Use CTkButton instead of tkinter Button
+# Setup main window navigation
 
 button = ct.CTkButton(master=app, text="MyContacts", command=contacts_btn)
 button.place(relx=0.35, rely=0.1, anchor=tkinter.CENTER)
-
 
 button = ct.CTkButton(master=app, text="MyMap", command=map_btn)
 button.place(relx=0.65, rely=0.1, anchor=tkinter.CENTER)
@@ -52,8 +42,18 @@ button.place(relx=0.65, rely=0.1, anchor=tkinter.CENTER)
 button = ct.CTkButton(master=app, text="Exit", command=exit)
 button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
+# Call Entry and Search
+
+call_entry = ct.CTkEntry(master=app, height=50, width=100, placeholder_text="CALL SIGN", fg_color="white", text_color="black")
+call_entry.place(relx=.4, rely=.5, anchor = tkinter.CENTER)
+
+call_button = ct.CTkButton(master=app, text="Search", command=call_search, width=50, height=50)
+call_button.place(relx=0.55, rely=0.5, anchor=tkinter.CENTER)
+
+call_entry.bind('<Return>', call_search)
 
 
-
+#name_entry = ct.CTkEntry(master=app, height=10, width=150, placeholder_text="CALL SIGN", fg_color="white", text_color="black")
+#call_entry.place(relx=.2, rely=.5, anchor = tkinter.CENTER)
 
 app.mainloop()
