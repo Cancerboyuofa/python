5eb1543b55fd8db1af35363e790229c7b340cd67
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-02-26 12:55:03 -0700

    removed seperate functions py

--- MyQSO/functions.py
+++ MyQSO/functions.py
@@ -1,43 +1,3 @@
-import os
-import requests
-from requests.exceptions import HTTPError
-import json
-import tkinter
-import customtkinter as ct
-
-base_folder = os.path.dirname(__file__)
-bg_path = os.path.join(base_folder, 'myqso_bg.png')
-logo_path = os.path.join(base_folder, 'myqso_logo.png')
-
-ct.set_appearance_mode("dark")  # Modes: system (default), light, dark
-ct.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
-app = ct.CTk()  # create CTk window like you do with the Tk window
-app.geometry("600x400")
-app.resizable(False, False)
-app.title("MyQSO")
-
-
-# Add background
-bg = tkinter.PhotoImage(file = bg_path)
-label1 = tkinter.Label(app, image = bg, text="")
-label1.place(x = 0, y = 0)
-
-
-  
-# Setup main window navigation buttons
-
-#header = ct.CTkLabel(master=app, text="MyQSO", text_color="white", font=("Arial", 46))
-#header.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
-
-#button = ct.CTkButton(master=app, text="MyContacts", command=contacts_btn)
-#button.place(relx=0.35, rely=0.8, anchor=tkinter.CENTER)
-
-#button = ct.CTkButton(master=app, text="MyMap", command=map_btn)
-#button.place(relx=0.65, rely=0.8, anchor=tkinter.CENTER)
-
-button = ct.CTkButton(master=app, text="Exit", command=exit)
-button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
 # Functions
 
@@ -102,15 +62,4 @@ def call_search(*args):
             result_label.place(relx=.5, rely=.8, anchor=tkinter.CENTER)
             result_label.after(3000, result_label.destroy)
 
-# Call Entry and Search
 
-call_entry = ct.CTkEntry(master=app, height=50, width=100, placeholder_text="CALL SIGN", fg_color="white", text_color="black")
-call_entry.place(relx=.45, rely=.5, anchor = tkinter.CENTER)
-
-call_button = ct.CTkButton(master=app, text="Search", command=call_search, width=50, height=50)
-call_button.place(relx=0.6, rely=0.5, anchor=tkinter.CENTER)
-
-call_entry.bind('<Return>', call_search)
-
-
-app.mainloop()
