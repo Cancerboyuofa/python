4584d2c63cb619493e285fe8bd5f712c69706cdc
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-02-26 22:33:47 -0700

    Validity and DB checking

--- MyQSO/main.py
+++ MyQSO/main.py
@@ -1,4 +1,6 @@
 import os
+import time
+import datetime
 import sqlite3
 import requests
 from requests.exceptions import HTTPError
@@ -6,6 +8,12 @@ import json
 import tkinter
 import customtkinter as ct
 
+# Get Current Date and Time
+
+unix = int(time.time())
+date = str(datetime.datetime.fromtimestamp(unix).strftime('%m-%d-%Y %H:%M'))
+
+# Begin GUI
 
 ct.set_appearance_mode("dark")  # Modes: system (default), light, dark
 ct.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
@@ -36,7 +44,8 @@ c = conn.cursor()
 
 c.execute("""CREATE TABLE IF NOT EXISTS callsigns (
     
-    sign TEXT, 
+    id INTEGER PRIMARY KEY,
+    callsign TEXT, 
     first_name TEXT,
     last_name TXEXT,
     class TEXT,
@@ -70,10 +79,19 @@ def get_data(sign):
         api = json.loads(api_request.content)
         f_name = api['hamdb']['callsign']['fname']
         l_name = api['hamdb']['callsign']['name']
+        level = api['hamdb']['callsign']['class']
+        address = api['hamdb']['callsign']['addr1']
+        city = api['hamdb']['callsign']['addr2']
+        state = api['hamdb']['callsign']['state']
         zip = api['hamdb']['callsign']['zip']
-        return [f_name, l_name, zip]
+        country = api['hamdb']['callsign']['country']
+        lat = api['hamdb']['callsign']['lat']
+        lon = api['hamdb']['callsign']['lon']
+        grid = api['hamdb']['callsign']['grid']
+
+        return [f_name, l_name, level, address, city, state, zip, country, lat, lon, grid]
     
-    # Error Checking
+    # HTTP Error Checking
 
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
@@ -81,51 +99,93 @@ def get_data(sign):
         print(f'Other error occurred: {err}')
 
 
-# Check callsign box for errors and then if good, request data
+# Check callsign for errors
 
-def call_search(*args):
+def callsign_valid(*args):
+
     callsign = call_entry.get()
 
     if len(callsign) <= 1 or callsign.isalnum() == False:
         callsign = "ERROR: MISSING OR BAD CALLSIGN"
         result_label = ct.CTkLabel(master=app, text=callsign, text_color="RED", font=("Arial", 16))
         result_label.place(relx=.5, rely=.75, anchor=tkinter.CENTER)
-        result_label.after(2000, result_label.destroy)
+        result_label.after(1500, result_label.destroy)
+    else:
+        call_entry.delete(0,8)
+        db_search(callsign)
+        
+
+
+# Check DB for callsign locally
+
+def db_search(callsign):
+    
+    c.execute("SELECT first_name, state, last_contact FROM callsigns WHERE callsign = (?)", [callsign])
+    matches = c.fetchall()
+    
+    if matches:
+        found = True
+        result_label = ct.CTkLabel(master=app, text="Match Found! \n\n" + matches[0][0] + " from: " + matches[0][1] + "\n Your last QSO was on " + matches[0][2], text_color="white", font=("Arial", 14))
+        result_label.place(relx=.5, rely=.65, anchor=tkinter.CENTER)
+        result_label.after(3000, result_label.destroy)
+
 
     else:
-        callsign = call_entry.get()
-        data = get_data(callsign)
-        first = data[0]
-        last = data[1]
-        name = str(first) + " " + str(last)
-        zip = str(data[2])
+        found = False
+        result_label = ct.CTkLabel(master=app, text="No prior contacts, searching online...", text_color="white", font=("Arial", 14))
+        result_label.place(relx=.5, rely=.7, anchor=tkinter.CENTER)
+        result_label.after(2000, result_label.destroy)
+        app.after(2000, lambda: call_search(callsign, found))
 
 
-# Check if callsign in DB or not
+def call_search(callsign, found):
+    
+    data = get_data(callsign)
+    first = data[0]
+    last = data[1]
+    level = data[2]
+    street_addr = data[3]
+    city = data[4]
+    state = data[5]
+    zip = data[6]
+    country = data[7]
+    lat = data[8]
+    lon = data[9]
+    grid = data[10]
+
+        # Check if callsign in DB or not
+
+    if not zip == "NOT_FOUND" and found == False:
         
-        if zip == "NOT_FOUND":
-            result_label = ct.CTkLabel(master=app, text=callsign + " Not Found in DB", text_color="white", font=("Arial", 14))
-            result_label.place(relx=.5, rely=.7, anchor=tkinter.CENTER)
-            result_label.after(3000, result_label.destroy)
+        c.execute("INSERT INTO callsigns(callsign, first_name, last_name, class, address, city, state, zip_code, country, map_lat, map_lon, map_grid, last_contact) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                (callsign.upper(), first.title(), last.title(), level, street_addr, city.title(), state.upper(), zip, country.title(), lat, lon, grid.upper(), date)
+                )
+                
+        conn.commit()
 
+        # Parse and format callsign data
 
-# Parse and format callsign data
+        name = str(first) + " " + str(last)
+        result_label = ct.CTkLabel(master=app, text=str(callsign) + "'s Name is: " + name + "\n", text_color="white", font=("Arial", 14))
+        result_label.place(relx=.5, rely=.7, anchor=tkinter.CENTER)
+        result_label.after(3000, result_label.destroy)
+        state_msg = "Located in: "
+        result_label = ct.CTkLabel(master=app, text=state_msg + state, text_color="white", font=("Arial", 14))
+        result_label.place(relx=.5, rely=.8, anchor=tkinter.CENTER)
+        result_label.after(3000, result_label.destroy)
 
-        else:
-            result_label = ct.CTkLabel(master=app, text=str(callsign) + "'s Name is: " + name, text_color="white", font=("Arial", 14))
-            result_label.place(relx=.5, rely=.7, anchor=tkinter.CENTER)
-            result_label.after(3000, result_label.destroy)
+    else:
+        result_label = ct.CTkLabel(master=app, text=" Sorry, " + callsign + " not found online", text_color="white", font=("Arial", 14))
+        result_label.place(relx=.5, rely=.7, anchor=tkinter.CENTER)
+        result_label.after(2000, result_label.destroy)             
 
-            result_label = ct.CTkLabel(master=app, text="The zip code is: " + zip, text_color="white", font=("Arial", 14))
-            result_label.place(relx=.5, rely=.8, anchor=tkinter.CENTER)
-            result_label.after(3000, result_label.destroy)
-
+           
 
 # Setup Text buttons
 call_entry = ct.CTkEntry(master=app, height=50, width=100, placeholder_text="CALL SIGN", fg_color="white", text_color="black")
 call_entry.place(relx=.45, rely=.5, anchor = tkinter.CENTER)
 
-call_button = ct.CTkButton(master=app, text="Search", command=call_search, width=50, height=50)
+call_button = ct.CTkButton(master=app, text="Search", command=callsign_valid, width=50, height=50)
 call_button.place(relx=0.6, rely=0.5, anchor=tkinter.CENTER)
 
 
@@ -135,8 +195,8 @@ button.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
 
 
 # Bind callsign box to enter key
-call_entry.bind('<Return>', call_search)
+call_entry.bind('<Return>', callsign_valid)
 
 
-# End of tkinter app
+# End tkinter app
 app.mainloop()
